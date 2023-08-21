329ef54f8e913844f2efad11f23d76c85c286a85
UGUIS8020 <shibuyamasahiko@gmail.com>
2023-08-22 01:21:03 +0900

    n001211を追加

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__, static_folder='./templates/images')
 def home():
     return render_template('index.html')
 
+@app.route('/n001211')
+def n001211():
+    return render_template('n001211.html')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
