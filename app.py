94981d9d02048155ebeee6ea3beb03683866f4cb
UGUIS8020 <shibuyamasahiko@gmail.com>
2023-08-22 17:21:13 +0900

    no message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder='./templates/images')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title = 'index.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
