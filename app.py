2dfdb55daf1385b30a2a153b81115d354d9ce6d0
UGUIS8020 <shibuyamasahiko@gmail.com>
2024-01-24 21:50:27 +0900

    no message

--- app.py
+++ app.py
@@ -3,13 +3,37 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,Email,EqualTo
 
+import os
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__, static_folder='./templates/images')
 
 app.config['SECRET_KEY'] = 'mysecretkey'
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(64),unique=True,index=True)
+    email = db.Column(db.String(64),unique=True,index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self,username,email,password_hash):
+        self.username = username
+        self.email = email
+        self.password_hash = password_hash
+
+    def __repr__(self):
+        return f"Username {self.username}"
+
 class RegistrationForm(FlaskForm):
     username = StringField('ユーザー名',validators=[DataRequired()])
-    email = StringField('メールアドレス', validators=[DataRequired(), Email(massage='正しいメールアドレスを入力してください')])
+    email = StringField('メールアドレス', validators=[DataRequired(), Email(message='正しいメールアドレスを入力してください')])
     email_confirm = StringField('メールアドレス(確認)', validators=[DataRequired(), EqualTo('email', message='メールアドレスが一致しません')])
     password = PasswordField('パスワード', validators=[DataRequired(), EqualTo('pass_confirm', message='パスワードが一致しません')])
     pass_confirm = PasswordField('パスワード(確認)', validators=[DataRequired()])
