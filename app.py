10545c444c49906f188264cec03379115110ac31
UGUIS8020 <shibuyamasahiko@gmail.com>
2024-01-29 23:38:53 +0900

    新越谷

--- app.py
+++ app.py
@@ -20,29 +20,39 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app,db)
 
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(64),unique=True,index=True)
     email = db.Column(db.String(64),unique=True,index=True)
     password_hash = db.Column(db.String(128))
-    adminisrator = db.Column(db.String(1))
+    administrator = db.Column(db.String(1))
+    post = db.relationship('BlogPost',backref='author',lazy='dynamic')
 
-    def __init__(self,username,email,password_hash,adminisrator):
+    def __init__(self,username,email,password_hash,administrator):
         self.username = username
         self.email = email
         self.password_hash = password_hash
-        self.adminisrator = adminisrator
+        self.administrator = administrator
 
     def __repr__(self):
         return f"Username {self.username}"
 class BlogPost(db.Model):
-    __tablename__ = 'blogposts'
+    __tablename__ = 'blog_post'
     id = db.Column(db.Integer,primary_key=True)
-    user_id = db.Column(db.Integer,db.Foreignkey('users.id'))
-    date = db.Column(db.DateTime,default=datetime.now('Asia/Tokyo'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    date = db.Column(db.DateTime,default=datetime.now(timezone('Asia/Tokyo')))
     title = db.Column(db.String(140))
-    text = db.Column(db.text)
+    text = db.Column(db.Text)
     summary = db.Column(db.String(140))
     featured_image = db.Column(db.String(140))
 
@@ -54,7 +64,7 @@ class BlogPost(db.Model):
         self.summary = summary
 
     def __repr__(self):
-        return f"PostID:{self.id},Title: {self.title}\n"
+        return f"PostID:{self.id},Title: {self.title}, Author: {self.author}\n"
 
 
     def __repr__(self):
