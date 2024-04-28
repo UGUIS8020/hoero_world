d16b1b180713c9ba0d00b1d59c76856579f560c6
UGUIS8020 <shibuyamasahiko@gmail.com>
2024-04-29 00:46:07 +0900

    新越谷　flask_app

--- company_blog/__init__.py
+++ company_blog/__init__.py
@@ -4,8 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-
-app = Flask(__name__, static_folder='./templates/images')
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mysecretkey'
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -13,7 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-Migrate(app,db)
+Migrate(app, db)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -22,7 +21,6 @@ login_manager.login_view = 'users.login'
 def localize_callback(*args, **kwargs):
     return 'このページにアクセスするには、ログインが必要です。'
 login_manager.localize_callback = localize_callback
-
 
 from sqlalchemy.engine import Engine
 from sqlalchemy import event
--- company_blog/models.py
+++ company_blog/models.py
@@ -5,40 +5,38 @@ from flask_login import UserMixin
 
 from company_blog import db, login_manager
 
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
-
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     administrator = db.Column(db.String(1))
     post = db.relationship('BlogPost', backref='author', lazy='dynamic')
 
-    def __init__(self, username, email, password, administrator):
-        self.username = username
+    def __init__(self, email, username, password, administrator):
         self.email = email
+        self.username = username
         self.password = password
         self.administrator = administrator
 
     def __repr__(self):
-        return f"Username: {self.username}"
-    
-    def check_password(self,password):
+        return f"UserName: {self.username}"
+
+    def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
     @property
     def password(self):
-        raise AttributeError('パスワードは読み取り専用です')
-    
+        raise AttributeError('password is not a readable attribute')
+
     @password.setter
-    def password(self,password):
+    def password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def is_administrator(self):
@@ -47,11 +45,12 @@ class User(db.Model, UserMixin):
         else:
             return 0
 
-    
-class BlogPost(db.Model):
-    __tablename__ = 'blogposts'
+    def count_posts(self, userid):
+        return BlogPost.query.filter_by(user_id=userid).count()
 
-    id = db.Column(db.Integer, primary_key = True)
+class BlogPost(db.Model):
+    __tablename__ = 'blog_post'
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     category_id = db.Column(db.Integer, db.ForeignKey('blog_category.id'))
     date = db.Column(db.DateTime, default=datetime.now(timezone('Asia/Tokyo')))
@@ -60,45 +59,46 @@ class BlogPost(db.Model):
     summary = db.Column(db.String(140))
     featured_image = db.Column(db.String(140))
 
-    def __init__(self, title, text, featured_image,user_id, category_id, summary):
+    def __init__(self, title, text, featured_image, user_id, category_id, summary):
         self.title = title
         self.text = text
         self.featured_image = featured_image
         self.user_id = user_id
         self.category_id = category_id
-        self.summary = summary        
-    
+        self.summary = summary
+
     def __repr__(self):
-        return f"PostID: {self.id},Title: {self.title},Author: {self.author} \n"
-    
+        return f"PostID: {self.id}, Title: {self.title}, Author: {self.author} \n"
+
 class BlogCategory(db.Model):
     __tablename__ = 'blog_category'
-    
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(140))
     posts = db.relationship('BlogPost', backref='blogcategory', lazy='dynamic')
-    
+
     def __init__(self, category):
         self.category = category
     
     def __repr__(self):
-        return f"CategoryID: {self.id},CategoryName: {self.category} \n"
+        return f"CategoryID: {self.id}, CategoryName: {self.category} \n"
+
+    def count_posts(self, id):
+        return BlogPost.query.filter_by(category_id=id).count()
 
 class Inquiry(db.Model):
-    __tablename__ = 'inquiriry'
-    
-    id = db.Column(db.Integer, primary_key = True)
+    __tablename__ = 'inquiry'
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     email = db.Column(db.String(64))
     title = db.Column(db.String(140))
     text = db.Column(db.Text)
     date = db.Column(db.DateTime, default=datetime.now(timezone('Asia/Tokyo')))
-    
+
     def __init__(self, name, email, title, text):
         self.name = name
         self.email = email
         self.title = title
         self.text = text
-    
+
     def __repr__(self):
         return f"InquiryID: {self.id}, Name: {self.name}, Text: {self.text} \n"
