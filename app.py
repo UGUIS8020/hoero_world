cbdd807d829b81bd6bfba80eaecfa53fb8406f7a
UGUIS8020 <shibuyamasahiko@gmail.com>
2025-04-05 21:29:30 +0900

    新越谷

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from extensions import db, login_manager
+from dotenv import load_dotenv
+
+load_dotenv()
 
 flask_app = Flask(__name__)
 
