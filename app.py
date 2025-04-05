391432e65febf97afc78533ff8996ac1d82d3c0e
UGUIS8020 <shibuyamasahiko@gmail.com>
2025-04-05 22:33:22 +0900

    新越谷

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from extensions import db, login_manager
 from dotenv import load_dotenv
-
 load_dotenv()
 
 flask_app = Flask(__name__)
